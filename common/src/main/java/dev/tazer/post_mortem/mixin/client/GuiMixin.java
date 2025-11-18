87198b608a5040639ed60ab33510fb51cb5c0c62
tazercopter <yaseenelgamal20094@gmail.com>
2025-11-18 20:00:41 +0200

    I figured out multiloader enum extensions

--- common/src/main/java/dev/tazer/post_mortem/mixin/client/GuiMixin.java
+++ common/src/main/java/dev/tazer/post_mortem/mixin/client/GuiMixin.java
@@ -1,9 +1,8 @@
 package dev.tazer.post_mortem.mixin.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.tazer.post_mortem.PostMortem;
 import dev.tazer.post_mortem.client.SpiritGui;
-import dev.tazer.post_mortem.entity.DeadHeartType;
 import dev.tazer.post_mortem.entity.SoulState;
 import dev.tazer.post_mortem.mixininterface.client.GuiExtension;
 import net.minecraft.client.DeltaTracker;
@@ -12,8 +11,6 @@ import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.DeathScreen;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.util.Mth;
-import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,6 +19,7 @@ import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
@@ -31,13 +29,6 @@ public abstract class GuiMixin implements GuiExtension {
     @Final
     private Minecraft minecraft;
 
-    @Shadow
-    @Final
-    private RandomSource random;
-
-    @Shadow
-    protected abstract void renderHeart(GuiGraphics guiGraphics, Gui.HeartType heartType, int x, int y, boolean hardcore, boolean halfHeart, boolean blinking);
-
     @Unique
     private SpiritGui spiritGui;
 
@@ -46,75 +37,80 @@ public abstract class GuiMixin implements GuiExtension {
         spiritGui = new SpiritGui(minecraft);
     }
 
-    @Inject(method = "renderHearts", at = @At(value = "HEAD"), cancellable = true)
-    private void maybeRenderHearts(GuiGraphics graphics, Player player, int x, int y, int height, int offsetHeartIndex, float maxHealth, int currentHealth, int displayHealth, int absorptionAmount, boolean renderHighlight, CallbackInfo ci) {
-        if (player.getSoulState() != SoulState.ALIVE) {
-            renderDeadHearts(graphics, player, x, y, height, offsetHeartIndex, maxHealth, currentHealth, displayHealth, absorptionAmount, renderHighlight);
-            ci.cancel();
-        }
+    @ModifyVariable(method = "renderHearts", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private int modifyX(int x) {
+        return minecraft.player.getSoulState() == SoulState.DOWNED || minecraft.screen instanceof DeathScreen ? x+78 : x;
     }
 
-    @Unique
-    private void renderDeadHearts(GuiGraphics graphics, Player player, int x, int y, int height, int offsetHeartIndex, float maxHealth, int currentHealth, int displayHealth, int absorptionAmount, boolean renderHighlight) {
-        boolean hardcore = player.level().getLevelData().isHardcore();
-
-        if (player.getSoulState() == SoulState.DOWNED || minecraft.screen instanceof DeathScreen) {
-            hardcore = true;
-            maxHealth = currentHealth;
-            x += 78;
-        }
-
-//        x = graphics.guiWidth() / 2 - hearts * 4;
-
-        DeadHeartType heartType = DeadHeartType.forPlayer(player);
-        int hearts = Mth.ceil(maxHealth / 2);
-        int absorptionHearts = Mth.ceil(absorptionAmount / 2F);
-        int halfHearts = hearts * 2;
-        if (minecraft.player.jumpableVehicle() == null) y += 5;
-
-        for (int l = hearts + absorptionHearts - 1; l >= 0; l--) {
-            int i1 = l / 10;
-            int j1 = l % 10;
-            int k1 = x + j1 * 8;
-            int l1 = y - i1 * height;
-            if (currentHealth + absorptionAmount <= 4) {
-                l1 += random.nextInt(2);
-            }
-
-            if (l < hearts && l == offsetHeartIndex) {
-                l1 -= 2;
-            }
-
-            renderHeart(graphics, Gui.HeartType.CONTAINER, k1, l1, hardcore, renderHighlight, false);
-            int i2 = l * 2;
-            boolean flag1 = l >= hearts;
-            if (flag1) {
-                int j2 = i2 - halfHearts;
-                if (j2 < absorptionAmount) {
-                    boolean flag2 = j2 + 1 == absorptionAmount;
-                    renderHeart(graphics, Gui.HeartType.ABSORBING, k1, l1, hardcore, false, flag2);
-                }
-            }
-
-            if (renderHighlight && i2 < displayHealth) {
-                boolean flag3 = i2 + 1 == displayHealth;
-                renderDeadHeart(graphics, heartType, k1, l1, true, flag3);
-            }
-
-            if (i2 < currentHealth) {
-                boolean flag4 = i2 + 1 == currentHealth;
-                renderDeadHeart(graphics, heartType, k1, l1, false, flag4);
-            }
-        }
-
+    @ModifyVariable(method = "renderHearts", at = @At("HEAD"), ordinal = 1, argsOnly = true)
+    private int modifyY(int y) {
+        return minecraft.player.getSoulState() != SoulState.ALIVE && minecraft.player.jumpableVehicle() == null ? y+5 : y;
     }
 
-    @Unique
-    private void renderDeadHeart(GuiGraphics guiGraphics, DeadHeartType heartType, int x, int y, boolean halfHeart, boolean blinking) {
-        RenderSystem.enableBlend();
-        guiGraphics.blitSprite(heartType.getSprite(blinking, halfHeart), x, y, 9, 9);
-        RenderSystem.disableBlend();
+    @ModifyVariable(method = "renderHearts", at = @At("HEAD"), ordinal = 0, argsOnly = true)
+    private float modifyMaxHealth(float maxHealth, @Local(argsOnly = true, ordinal = 4) int currentHealth) {
+        return minecraft.player.getSoulState() == SoulState.DOWNED || minecraft.screen instanceof DeathScreen ? currentHealth : maxHealth;
     }
+
+//    @Unique
+//    private void renderDeadHearts(GuiGraphics graphics, Player player, int x, int y, int height, int offsetHeartIndex, float maxHealth, int currentHealth, int displayHealth, int absorptionAmount, boolean renderHighlight) {
+//        boolean hardcore = player.level().getLevelData().isHardcore();
+//
+//        if (player.getSoulState() == SoulState.DOWNED || minecraft.screen instanceof DeathScreen) {
+//            hardcore = true;
+//            maxHealth = currentHealth;
+//            x += 78;
+//        }
+//
+//        DeadHeartType heartType = DeadHeartType.forPlayer(player);
+//        int hearts = Mth.ceil(maxHealth / 2);
+//        int absorptionHearts = Mth.ceil(absorptionAmount / 2F);
+//        int halfHearts = hearts * 2;
+//        if (minecraft.player.jumpableVehicle() == null) y += 5;
+//
+//        for (int l = hearts + absorptionHearts - 1; l >= 0; l--) {
+//            int i1 = l / 10;
+//            int j1 = l % 10;
+//            int k1 = x + j1 * 8;
+//            int l1 = y - i1 * height;
+//            if (currentHealth + absorptionAmount <= 4) {
+//                l1 += random.nextInt(2);
+//            }
+//
+//            if (l < hearts && l == offsetHeartIndex) {
+//                l1 -= 2;
+//            }
+//
+//            renderHeart(graphics, Gui.HeartType.CONTAINER, k1, l1, hardcore, renderHighlight, false);
+//            int i2 = l * 2;
+//            boolean flag1 = l >= hearts;
+//            if (flag1) {
+//                int j2 = i2 - halfHearts;
+//                if (j2 < absorptionAmount) {
+//                    boolean flag2 = j2 + 1 == absorptionAmount;
+//                    renderHeart(graphics, Gui.HeartType.ABSORBING, k1, l1, hardcore, false, flag2);
+//                }
+//            }
+//
+//            if (renderHighlight && i2 < displayHealth) {
+//                boolean flag3 = i2 + 1 == displayHealth;
+//                renderDeadHeart(graphics, heartType, k1, l1, true, flag3);
+//            }
+//
+//            if (i2 < currentHealth) {
+//                boolean flag4 = i2 + 1 == currentHealth;
+//                renderDeadHeart(graphics, heartType, k1, l1, false, flag4);
+//            }
+//        }
+//
+//    }
+
+//    @Unique
+//    private void renderDeadHeart(GuiGraphics guiGraphics, DeadHeartType heartType, int x, int y, boolean halfHeart, boolean blinking) {
+//        RenderSystem.enableBlend();
+//        guiGraphics.blitSprite(heartType.getSprite(blinking, halfHeart), x, y, 9, 9);
+//        RenderSystem.disableBlend();
+//    }
 
     @ModifyArg(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blitSprite(Lnet/minecraft/resources/ResourceLocation;IIII)V", ordinal = 1))
     private ResourceLocation changeAttackIndicator(ResourceLocation sprite) {
---
+++ common/src/main/java/dev/tazer/post_mortem/mixin/client/HeartTypeEnumExtension.java
@@ -0,0 +1,62 @@
+package dev.tazer.post_mortem.mixin.client;
+
+import dev.tazer.post_mortem.PostMortem;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Mixin(Gui.HeartType.class)
+public abstract class HeartTypeEnumExtension {
+
+    @Shadow
+    @Final
+    @Mutable
+    private static Gui.HeartType[] $VALUES;
+
+    @Unique
+    private static final Gui.HeartType DOWNED = addHeartType("DOWNED", ResourceLocation.withDefaultNamespace("hud/heart/hardcore"));
+    @Unique
+    private static final Gui.HeartType MANIFESTATION = addHeartType("MANIFESTATION", PostMortem.location("hearts/manifestation_heart"));
+    @Unique
+    private static final Gui.HeartType POSSESSION = addHeartType("POSSESSION", PostMortem.location("hearts/possession_heart"));
+
+    @Inject(method = "<init>", at = @At("TAIL"))
+    private void c(String full, int fullBlinking, ResourceLocation half, ResourceLocation halfBlinking, ResourceLocation hardcoreFull, ResourceLocation hardcoreBlinking, ResourceLocation hardcoreHalf, ResourceLocation hardcoreHalfBlinking, ResourceLocation par9, ResourceLocation par10, CallbackInfo ci) {
+    }
+
+    @Invoker("<init>")
+    private static Gui.HeartType invokeInit(String name, int id, ResourceLocation full, ResourceLocation fullBlinking, ResourceLocation half, ResourceLocation halfBlinking, ResourceLocation hardcoreFull, ResourceLocation hardcoreBlinking, ResourceLocation hardcoreHalf, ResourceLocation hardcoreHalfBlinking) {
+        throw new AssertionError();
+    }
+
+    @Unique
+    private static Gui.HeartType addHeartType(String name, ResourceLocation heart) {
+        ArrayList<Gui.HeartType> heartTypes = new ArrayList<>(Arrays.asList($VALUES));
+        ResourceLocation fullHeart = heart.withSuffix("_full");
+        ResourceLocation halfHeart = heart.withSuffix("_half");
+        ResourceLocation fullBlinking = fullHeart.withSuffix("_blinking");
+        ResourceLocation halfBlinking = halfHeart.withSuffix("_blinking");
+        Gui.HeartType heartType = invokeInit(name, heartTypes.getLast().ordinal() + 1, fullHeart, fullBlinking, halfHeart, halfBlinking, fullHeart, fullBlinking, halfHeart, halfBlinking);
+        heartTypes.add(heartType);
+        $VALUES = heartTypes.toArray(new Gui.HeartType[0]);
+        return heartType;
+    }
+
+    @Inject(method = "forPlayer", at = @At("HEAD"), cancellable = true)
+    private static void forPlayer(Player player, CallbackInfoReturnable<Gui.HeartType> cir) {
+        switch (player.getSoulState()) {
+            case MANIFESTATION -> cir.setReturnValue(MANIFESTATION);
+            case POSSESSION -> cir.setReturnValue(POSSESSION);
+            case DOWNED -> cir.setReturnValue(DOWNED);
+        }
+    }
+}
